add02ded66ff73632ad5cd6514429800a7db1140
Rafael Williams <rafael.williams@gmail.com>
2013-03-19 15:08:54 -0500

    add more test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,5 +44,18 @@ describe "User pages" do
 	        expect { click_button submit }.to change(User, :count).by(1)
 	      end
 	    end
+	   describe "after submission" do
+	        before { click_button submit }
+
+	        it { should have_selector('title', text: 'Sign up') }
+	        it { should have_content('error') }
+      end
+       describe "after saving the user" do
+	        before { click_button submit }
+	        let(:user) { User.find_by_email('user@example.com') }
+
+	        it { should have_selector('title', text: user.name) }
+	        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
 end
