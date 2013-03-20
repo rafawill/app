cdf1744eaa9e342c00e2813e2df3bd4d4f8dc322
Rafael Williams <rafael.williams@gmail.com>
2013-03-20 09:10:15 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) } 
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -84,5 +86,9 @@ describe User do
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
+  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
